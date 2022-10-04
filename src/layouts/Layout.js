5457701ee49c72daf4ce8f6f2caede61737706c2
Doddy MATABARO <matabarododdy@gmail.com>
2022-10-04 14:59:53 +0200

    peojects view

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,6 +1,10 @@
-import { DarkThemeToggle, Flowbite, Navbar } from 'flowbite-react';
+import { DarkThemeToggle, Flowbite, Footer, Navbar } from 'flowbite-react';
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import {footer} from '../assets/data'
+import logo from '../assets/logook.svg'
+import { BsFacebook, BsInstagram, BsTwitter, BsLinkedin } from 'react-icons/bs'
+
 
 function Layout() {
   return (
@@ -9,7 +13,7 @@ function Layout() {
             fluid={true}
             rounded={true}
             >
-            <Navbar.Brand href="https://flowbite.com/">
+            <Navbar.Brand href="#">
                 <img
                 src="https://flowbite.com/docs/images/logo.svg"
                 className="mr-3 h-6 sm:h-9"
@@ -40,6 +44,84 @@ function Layout() {
             </Navbar>
             
             <Outlet/>
+
+            {/* footer */}
+            <Footer container={true}>
+  <div className="w-full">
+    <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+      <div>
+        <Footer.Brand
+          href="https://flowbite.com"
+          src={logo}
+          alt="ECR logo"
+          name="ECR sarl"
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+        <div>
+          <Footer.Title title="Apropos" />
+          <Footer.LinkGroup col={true}>
+            <Footer.Link href="/about">
+              E.C.R sarl
+            </Footer.Link>
+            <Footer.Link href="https://geniecivilpdf.com/pdf">
+              Genie civil
+            </Footer.Link>
+          </Footer.LinkGroup>
+        </div>
+        <div>
+          <Footer.Title title="Suivrez-nous sur : " />
+          <Footer.LinkGroup col={true}>
+            <Footer.Link href="#">
+              Facebook
+            </Footer.Link>
+            <Footer.Link href="#">
+              Linkdin
+            </Footer.Link>
+          </Footer.LinkGroup>
+        </div>
+        <div>
+          <Footer.Title title="Mentions legales" />
+          <Footer.LinkGroup col={true}>
+            <Footer.Link href="#">
+              Privacy Policy
+            </Footer.Link>
+            <Footer.Link href="#">
+              Terms & Conditions
+            </Footer.Link>
+          </Footer.LinkGroup>
+        </div>
+      </div>
+    </div>
+    <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="ECR sarl"
+            year={2022}
+          />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon
+              href="#"
+              icon={BsFacebook}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsInstagram}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsTwitter}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsLinkedin}
+            />
+
+          </div>
+        </div>
+      </div>
+    </Footer>
     </>
   )
 }
--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,8 +1,69 @@
-import React from 'react'
+import { Card, Pagination } from 'flowbite-react'
+import { useState, useEffect } from 'react'
+import { projects } from '../assets/data'
 
 function Projects() {
+  const itemsPerPage = 10
+  const numberOfPages = Math.ceil(projects.length / itemsPerPage)
+  const paginate = (projectsItems) => {
+    const newProjectsItems = Array.from({ length: numberOfPages }, (_, index) => {
+      const start = index * itemsPerPage
+      return projectsItems.slice(start, start + itemsPerPage)
+    })
+    return newProjectsItems
+  }
+
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+  const getProjects = () => {
+    setData(paginate(projects))
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+      getProjects()
+  },[])
+  
+  const [page, setPage] = useState(0)
+  const [projectsWrapped, setProjectsWrapped] = useState([])
+ 
+  useEffect(() => {
+    if (loading) return
+    setProjectsWrapped(data[page])
+  }, [loading, page])
+
+  const handlePage = (index) => {
+    setPage(index)
+  }
+
+  console.log(projectsWrapped)
   return (
-    <div>Projects</div>
+    <Card>
+        <h5 className="mb-2 text-4xl  text-center  uppercase font-bold text-gray-900 dark:text-white">
+            Notre Gallerie
+        </h5>
+      <div className='grid grid-cols-4 gap-2 sm:grid-cols-1 md:grid-cols-2'>
+        {projectsWrapped.map((project)=>{
+          return(
+            <Card class="bg-inherit h-72 ">
+              <img class="w-full h-full rounded-lg object-scale-down transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0" src={project} alt="project" />
+            </Card>
+          )
+        })}
+      </div>
+      <div className="flex items-center justify-center text-center">
+        <Pagination
+          currentPage={page}
+          layout="paginate"
+          // onPageChange={getProducts()}
+          showIcons={true}
+          totalPages={numberOfPages}
+          previouslabel="Go back"
+          nextlabel="Go forward"
+          />
+      </div>
+    </Card>
+      
   )
 }
 
